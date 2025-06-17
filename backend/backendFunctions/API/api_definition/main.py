49e12a72d21c4fb688b6a5fb6d29c3a09cd7d869
Moritz Fleig <ublti@student.kit.edu>
2025-06-17 22:53:36 +0200

    Testing automatic deployment by render.com

--- backend/backendFunctions/API/api_definition/main.py
+++ backend/backendFunctions/API/api_definition/main.py
@@ -3,9 +3,9 @@ from routers import users, ai_model, statistics
 
 api = FastAPI(
     title="API für das Teamprojekt EDUKIT",
-    description="Diese API dient der Kommunikation von Frontend und Backend im Teamprojekt EDUKIT.Sie " \
+    description="Diese API dient der Kommunikation von Frontend und Backend im Teamprojekt EDUKIT. Sie " \
     "erlaubt den Umgang mit Nutzerdaten, Spielinhalten bzw. erzeugten Lernfragen sowie Spielstatistiken." \
-    "Diese Swagger-Documentation ist unter api.edukit-tp.me/docs erreichbar und ermöglicht eine Übersicht über die verschiedenen Endpunkte."
+    "Diese Swagger-Documentation ist unter api.edukit-tp.me/docs erreichbar und ermöglicht eine Übersicht über die verschiedenen Endpunkte.\n Unten sind alle Endpunkte aufgelistet: "
 )
 api.include_router(users.router)
 api.include_router(ai_model.router)
