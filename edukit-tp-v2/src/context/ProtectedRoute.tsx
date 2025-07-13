fdd38aca74802f38643879ea7ef663c191833449
Moritz Fleig <ublti@student.kit.edu>
2025-07-13 17:29:58 +0200

    Changed route protection

--- edukit-tp-v2/src/context/ProtectedRoute.tsx
+++ edukit-tp-v2/src/context/ProtectedRoute.tsx
@@ -1,10 +1,26 @@
 import { Navigate, Outlet } from "react-router-dom";
 import { useUser } from "./UserContext";
+import {useEffect, useState} from "react";
+import { auth } from "../firebaseData/firebaseConfig";
 
 const ProtectedRoute = () => {
-  const { user } = useUser();
+  const [verified, setVerified] = useState<boolean| null> (null);
 
-  if (!user) {
+  useEffect(() =>{
+    const unsubscribe = auth.onAuthStateChanged(async (user) => {
+    if (user){
+      await user.reload();
+      setVerified(user.emailVerified)
+    }else{
+      setVerified(false);
+    }
+    });
+    return () => unsubscribe();
+  }, []);
+
+  if (verified === null) {
+    return <div>Loading...</div>
+  }else if(!verified){
     return <Navigate to="/login" replace />;
   }
 
